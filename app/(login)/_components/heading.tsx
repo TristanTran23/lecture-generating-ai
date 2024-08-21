162dd3f74d1e27f78112bf3c2b825994030a313b
TristanTran23 <tranquangthinhtdn@gmail.com>
2024-08-21 10:42:43 -0400

    add navbar

--- app/(login)/_components/heading.tsx
+++ app/(login)/_components/heading.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Image } from "lucide-react";
+
 export const Heading = () => {
     return (
         <div className="max-w-3xl space-y-4">
-            <h1>
+            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
                 This is the heading
             </h1>
+            <Button className="text-xl">
+                Enter
+                <ArrowRight className="pl-2"/>
+            </Button>
         </div>
     )
 }
---
+++ app/(login)/_components/navbar.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export const NavBar = () => {
+    return (
+        
+        <div>
+            Navbar
+        </div>
+    )
+}
